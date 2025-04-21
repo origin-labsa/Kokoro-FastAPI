672ea4cb40302f7068130e7ba35a04854ece7877
Sifiso Madwayi <sifiso@originlab.co.za>
2025-04-22 00:07:42 +0200

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -4,9 +4,9 @@ set -e
 # Get version from argument or use default
 VERSION=${1:-"latest"}
 
-# Build both CPU and GPU images using docker buildx bake
-echo "Building CPU and GPU images..."
+# Build CPU image using docker buildx bake
+echo "Building CPU image..."
 VERSION=$VERSION docker buildx bake --push
 
 echo "Build complete!"
-echo "Created images with version: $VERSION"
+echo "Created image with version: $VERSION"
